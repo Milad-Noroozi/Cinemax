4ae843c383580ff95c759aacf84423f43e2d28f7
Milad Noroozi <miladi1338@gmail.com>
2025-07-05 22:40:34 +0330

    Add splash screen and initial home page design

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,14 +7,61 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF1A1A2E),
-
-      body: Center(
+      backgroundColor: const Color(0xFF1A1A2E),
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Milad",style: TextStyle(fontSize: 20, color: Colors.white),)
-          ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      CupertinoIcons.person_alt_circle,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hello, Smith",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          "Let’s stream your favorite movie",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 146, 146, 157),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Spacer(),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 37, 40, 54),
+                      borderRadius: BorderRadius.all(Radius.circular(15))
+                    ),
+                    child: Icon(CupertinoIcons.heart_fill,color: Colors.red,),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
